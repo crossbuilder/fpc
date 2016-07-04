9b7ed8b6dd998162598afd56a55e0242f665a086
crossbuilder <crossbuilder@users.noreply.github.com>
2016-07-04 08:56:56 +0000

    * install sources too + use bz2 tars

--- install.sh
+++ install.sh
@@ -8,8 +8,9 @@ echo "${BD}"
 sleep 5
 make svn centos arm
 cd /usr/
-tar xf ${BD}/dist/fpc-3.1.1.x86_64-linux.tar.gz 
-tar xf ${BD}/dist/arm-linux-fpc.x86_64-linux.tar.gz
+tar xf ${BD}/dist/fpc-3.1.1.x86_64-linux.tar.bz2 
+tar xf ${BD}/dist/arm-linux-fpc.x86_64-linux.tar.bz2
+tar xf ${BD}/dist/fpc.source.tar.bz2
 cd bin/
 ln -sf ../lib/fpc/3.1.1/ppcrossarm ppcrossarm
 ln -sf ../lib/fpc/3.1.1/ppcx64 ppcx64
