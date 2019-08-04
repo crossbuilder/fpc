a20fff8e7fb38108f091bf4244351bfc4ad81333
crossbuilder <crossbuilder@users.noreply.github.com>
2019-08-04 23:36:19 +0200

    * fpc trunk is now 3.3.1

--- install.sh
+++ install.sh
@@ -2,21 +2,23 @@
 #
 # PLEASE RUN AS ROOT!
 
+usever="3.3.1"
+
 cd $(dirname "$0")
 BD=$(pwd)
 echo "${BD}"
 sleep 5
 make svn centos arm
 cd /usr/
-tar xf ${BD}/dist/fpc-3.1.1.x86_64-linux.tar.bz2 
+tar xf ${BD}/dist/fpc-${usever}.x86_64-linux.tar.bz2 
 tar xf ${BD}/dist/arm-linux-fpc.x86_64-linux.tar.bz2
 tar xf ${BD}/dist/fpc.source.tar.bz2
 cd bin/
-ln -sf ../lib/fpc/3.1.1/ppcrossarm ppcrossarm
-ln -sf ../lib/fpc/3.1.1/ppcx64 ppcx64
+ln -sf ../lib/fpc/${usever}/ppcrossarm ppcrossarm
+ln -sf ../lib/fpc/${usever}/ppcx64 ppcx64
 cd ..
-lib/fpc/3.1.1/samplecfg 
-lib/fpc/3.1.1/samplecfg /usr/lib/fpc/3.1.1/ /etc
+lib/fpc/${usever}/samplecfg 
+lib/fpc/${usever}/samplecfg /usr/lib/fpc/${usever}/ /etc
 cd ${BD}
 cat add-fpc.cfg >> /etc/fpc.cfg
 
